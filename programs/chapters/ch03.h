220c9b56ce024b74af4129268352589cc7412875
Alberto Mansur <albertom2@al.insper.edu.br>
2024-09-03 09:33:36 -0300

    Aula 4

--- programs/chapters/ch03.h
+++ programs/chapters/ch03.h
@@ -3,6 +3,10 @@
 #include "src/utils/parameters.h"
 
 PwmOut motor(MOTOR1);
+PwmOut motor2(MOTOR2);
+PwmOut motor3(MOTOR3);
+PwmOut motor4(MOTOR4);
+
 
 float control_motor(float w)
 {
@@ -12,12 +16,22 @@ float control_motor(float w)
 
 int main()
 {
-    motor.period(1.0/500.0);
-    motor = control_motor(1000.0);
-    wait(10);
-    motor = 0.0;
     while (true)
     {
-        // Loop infinito, o código pode ser expandido aqui
+            // Loop infinito, o código pode ser expandido aqui
+        motor.period(1.0/500.0);
+        motor = control_motor(1600.0);
+        motor2.period(1.0/500.0);
+        motor2 = control_motor(1600.0);
+        motor3.period(1.0/500.0);
+        motor3 = control_motor(1600.0);
+        motor4.period(1.0/500.0);
+        motor4 = control_motor(1600.0);
+        wait(5.0);
+        motor = control_motor(0.0);
+        motor2 = control_motor(0.0);
+        motor3 = control_motor(0.0);
+        motor4 = control_motor(0.0);
+        wait(5.0);
     }
 }
